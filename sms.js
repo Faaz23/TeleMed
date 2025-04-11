fc09df58f4aa2537302193db1c55722f0d61ae1e
Faaz23 <136075768+Faaz23@users.noreply.github.com>
2025-04-11 10:28:24 +0530

    Update sms.js

--- sms.js
+++ sms.js
@@ -7,23 +7,21 @@ module.exports = async (req, res) => {
             const accountSid = process.env.TWILIO_ACCOUNT_SID;
             const authToken = process.env.TWILIO_AUTH_TOKEN;
             const fromNumber = process.env.TWILIO_FROM_NUMBER;
-            const toNumber = process.env.TO_NUMBER;
-
-            console.log('Twilio Credentials:', { accountSid, fromNumber, toNumber }); // Log credentials
+            const toNumber = req.body.to;
+            const body = req.body.body;
 
             const client = twilio(accountSid, authToken);
 
             const message = await client.messages.create({
-                body: 'Your SMS message here!',
+                body: body,
                 from: fromNumber,
                 to: toNumber,
             });
 
-            console.log('Twilio Message SID:', message.sid); // Log message SID
-
+            console.log(message.sid);
             res.status(200).send('SMS sent successfully!');
         } catch (error) {
-            console.error('Twilio Error:', error); // Log the error
+            console.error(error);
             res.status(500).send('Failed to send SMS.');
         }
     } else {
